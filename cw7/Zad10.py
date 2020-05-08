1399f24a608d979a7b0b7e792fbc0cc4e21d2990
michals-lab <59738119+michals-lab@users.noreply.github.com>
2020-05-08 14:33:37 +0200

    ss

---
+++ cw7/Zad10.py
@@ -0,0 +1,4 @@
+import numpy as nu
+x = nu.arange(6).reshape(2,3)
+print(x)
+print(x.reshape(3,2))
---
+++ cw7/Zad8.py
@@ -0,0 +1,4 @@
+import numpy as nu
+x = nu.arange(9).reshape(3,3)
+for i in range(3):
+    print(x[i,:])
